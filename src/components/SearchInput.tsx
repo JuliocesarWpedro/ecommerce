a45af063e00d25c1d14556ef7e7e449c1810f1de
Júlio César Wanderosfky Pedro <juliowanke@gmail.com>
2024-02-29 21:50:17 -0300

    feat: SearchInput and CartControl

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,5 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
+import { SearchIcon } from './icons/SearchIcon';
+
+const SearchContainer = styled.form`
+  display: flex;
+  background-color: var(--bg-secondary);
+  padding: 9px 16px;
+  font-size: 14px;
+  font-weight: 400;
+  border: none;
+  outline: none;
+  width: 352px;
+  height: 42px;
+  border-radius: 8px;
+  line-height: 22px;
+  letter-spacing: 0em;
+  align-items: center;
+  justify-content: space-between;
+
+  @media (max-width: 450px) {
+    width: 300px;
+  }
+
+  @media (max-width: 360px) {
+    width: 250px;
+    padding: 9px 9px;
+  }
+`;
 
 const TagInputSearch = styled.input`
   background-color: var(--bg-secondary);
@@ -8,6 +35,12 @@ const TagInputSearch = styled.input`
   border: none;
   outline: none;
   width: 280px;
+  @media (max-width: 450px) {
+    width: 228px;
+  }
+  @media (max-width: 360px) {
+    width: 200px;
+  }
   height: 42px;
   border-radius: 8px;
   line-height: 22px;
@@ -15,7 +48,32 @@ const TagInputSearch = styled.input`
 `;
 
 const SearchInput = () => {
-  return <TagInputSearch placeholder="Procurando por algo específico?" />;
+  const [inputValue, setInputValue] = React.useState<string>('');
+
+  function handleSubmit(value: string) {
+    console.log(value);
+  }
+
+  return (
+    <SearchContainer
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit(inputValue);
+      }}
+    >
+      <TagInputSearch
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Procurando por algo específico?"
+      />
+      <SearchIcon
+        onClick={() => {
+          handleSubmit(inputValue);
+        }}
+      />
+    </SearchContainer>
+  );
 };
 
 export default SearchInput;
